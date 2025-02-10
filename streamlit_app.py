85836c3a04e5b14cce33b788428a9721c60a87b5
Timothee-Audinet <81582248+Timothee-Audinet@users.noreply.github.com>
2025-02-10 23:04:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,7 +152,7 @@ if selected == "Accueil":
         st.markdown("""
         <div class="header-container">
             <img src="https://raw.githubusercontent.com/Timothee-Audinet/streamlit/refs/heads/main/LES%20JOURETS_08%202024-0718.jpg" alt="Bannière">
-            <div class="header-text">Timothée Audinet<br>Photographe Animalier</div>
+            <div class="header-text"><h1>Timothée Audinet</h1><h2>Photographe Animalier</h2></div>
         </div>
         """, unsafe_allow_html=True)
     
