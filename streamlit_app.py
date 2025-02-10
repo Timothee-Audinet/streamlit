d39ed62961fd4d331b4780ecdbbc91dfcbabdea4
Timothee-Audinet <81582248+Timothee-Audinet@users.noreply.github.com>
2025-02-10 22:27:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 #from PIL import Image
 #import os
 
@@ -12,22 +13,37 @@ st.sidebar.write("Passionné de photographie animalière, j'aime capturer la bea
 st.sidebar.markdown("[Mon Instagram](https://www.instagram.com/timothee_wildlife_photo/)")
 
 # Menu horizontal
-#menu = st.selectbox("Navigation", ["Photographie", "Wallpapers"])
-menu = st.selectbox("Navigation", ["Photographie", "Wallpapers"], key="menu_navigation")
+
+#menu = st.selectbox("Navigation", ["Photographie", "Wallpapers"], key="menu_navigation")
+selected = option_menu(
+    menu_title=None,  # Pas de titre principal
+    options=["ACCUEIL", "PHOTOGRAPHIE", "WALLPAPERS"],
+    icons=["house", "camera", "image"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="horizontal",
+    styles={
+        "container": {"padding": "0px", "background-color": "#333"},
+        "icon": {"color": "#bbb", "font-size": "18px"},
+        "nav-link": {"color": "#bbb", "font-size": "16px", "text-align": "center", "padding": "10px"},
+        "nav-link-selected": {"color": "white", "font-weight": "bold", "border-bottom": "2px solid white"},
+    }
+)
+
 
 # Définir les liens GitHub des dossiers d'images
 GITHUB_USERNAME = "Timothee-Audinet"
 REPO_NAME = "streamlit"
 BRANCH = "main"
-
 photo_dir = f"https://raw.githubusercontent.com/{GITHUB_USERNAME}/{REPO_NAME}/refs/heads/main/photos_dir/"
 wallpaper_dir = f"https://raw.githubusercontent.com/{GITHUB_USERNAME}/{REPO_NAME}/refs/heads/main/wallpaper_dir/"
-
 # Liste des images (à adapter selon ton repo)
 photo_files = ["BROCARDweb-4994.jpg", "MOYENDUC-1434-3.jpg", "OURSweb-6686-3.jpg"]  # Remplace par tes fichiers réels
 wallpaper_files = ["FondEcran-Gobemouche-3556.jpg"]  # Idem ici
 
-if menu == "Photographie":
+if selected == "ACCUEIL":
+    st.title("🏠 Bienvenue sur la page d'accueil")
+elif selected == "PHOTOGRAPHIE":
     st.title("📷 Galerie de Photographie")
     cols = st.columns(3)
     
@@ -37,7 +53,7 @@ if menu == "Photographie":
             st.image(img_url, use_container_width=True)
             st.caption(filename.split(".")[0])  # Affiche le nom du fichier
 
-elif menu == "Wallpapers":
+elif selected == "WALLPAPERS":
     st.title("🖼️ Téléchargez un Wallpaper")
     cols = st.columns(3)
     
