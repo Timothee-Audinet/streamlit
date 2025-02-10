0bb11250f2fd389cad240691ace8e5d6e5a33d7f
Timothee-Audinet <81582248+Timothee-Audinet@users.noreply.github.com>
2025-02-10 21:03:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.title('Wildlife screenshots')
 
+st.image("https://github.com/Timothee-Audinet/streamlit/edit/main/Wallpapers/sunrise.jpg", caption="Sunrise by the mountains")
+
 
