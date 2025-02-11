5883817ba4052b9a5c934937c193e0e71a380963
Timothee-Audinet <81582248+Timothee-Audinet@users.noreply.github.com>
2025-02-11 10:56:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.markdown(
         .css-18e3th9 {
             background-color: #333 !important;
             padding: 200px 0 !important;
-            width: 200% !important;
+            max-width: 1200% !important;
         }
         .css-18e3th9 span {
             font-size: 24px !important;
