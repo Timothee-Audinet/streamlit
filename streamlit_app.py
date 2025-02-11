e21d2f5601123964a265c1b3d5d5bee455332c49
Timothee-Audinet <81582248+Timothee-Audinet@users.noreply.github.com>
2025-02-11 10:45:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,30 +3,14 @@ from streamlit_navigation_bar import st_navbar
 
 st.set_page_config(layout="wide")
 
-pages = ["Home", "Install", "Documentation", "Examples", "GitHub"]
-styles = {
-    "nav": {
-        "background-color": "rgb(123, 209, 146)",
-    },
-    "div": {
-        "max-width": "32rem",
-    },
-    "span": {
-        "border-radius": "0.5rem",
-        "color": "rgb(49, 51, 63)",
-        "margin": "0 0.125rem",
-        "padding": "0.4375rem 0.625rem",
-    },
-    "active": {
-        "background-color": "rgba(255, 255, 255, 0.25)",
-    },
-    "hover": {
-        "background-color": "rgba(255, 255, 255, 0.35)",
-    },
-}
+pages = ["Accueil", "Galerie", "Fond d'écran", "Contact"],
+options = {"use_padding": False}
+urls = {"GitHub": "https://github.com/gabrieltempass/streamlit-navigation-bar"}
+styles = {"div": {"max-width": "35rem"}}
 
-page = st_navbar(pages, styles=styles)
-st.write(page)
+page = st_navbar(pages, urls=urls, options, styles=styles)
+
+st.header(page)
 
 #st.header(page)
 
