4c79d639ad49e9649e9c471a0ba51cc2b97eb92a
Timothee-Audinet <81582248+Timothee-Audinet@users.noreply.github.com>
2025-02-11 10:55:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ st.markdown(
             color: white;
         }
         .header-container img {
-            width: 1200%;
+            max-width: 1200%;
             height: auto;
             filter: brightness(60%);
         }
